a9fe452743a4f42939f64d014f080c78a93a5dba
Audrey Serene <audrey.serene@gmail.com>
2024-01-17 16:40:41 -0600

    Reorganize `AbilitySystem` methods, add activation and tick logic
    Also renames some `AbilitySystem` methods and properties for better clarity.

--- src/ability_system.cpp
+++ src/ability_system.cpp
@@ -1,36 +1,20 @@
 #include "ability_system.h"
 
 #include "macros.hpp"
+#include "utils.hpp"
 
-template <typename Ty, typename Fn>
-bool any(TypedArray<Ty> array, Fn fn) {
-	for (int i = 0; i < array.size(); i++) {
-		if (fn(array[i]))
-			return true;
-	}
-	return false;
-}
-
-template <typename Ty, typename Fn>
-bool all(TypedArray<Ty> array, Fn fn) {
-	for (int i = 0; i < array.size(); i++) {
-		if (!fn(array[i]))
-			return false;
-	}
-	return true;
-}
 void AbilitySystemState::_bind_methods() {
 	/* Bind getter/setter methods */
 	BIND_GETSET(AbilitySystemState, attribute_map);
-	BIND_GETSET(AbilitySystemState, granted_tags);
-	BIND_GETSET(AbilitySystemState, granted_abilities);
-	BIND_GETSET(AbilitySystemState, ability_events);
+	BIND_GETSET(AbilitySystemState, tags);
+	BIND_GETSET(AbilitySystemState, abilities);
+	BIND_GETSET(AbilitySystemState, events);
 
 	/* Bind properties */
 	OBJECT_PROP(AttributeMap, attribute_map);
-	PROP(Variant::ARRAY, granted_tags);
-	PROP(Variant::ARRAY, granted_abilities);
-	PROP(Variant::ARRAY, ability_events);
+	PROP(Variant::ARRAY, tags);
+	PROP(Variant::ARRAY, abilities);
+	PROP(Variant::ARRAY, events);
 }
 
 void AbilitySystem::_bind_methods() {
@@ -39,28 +23,105 @@ void AbilitySystem::_bind_methods() {
 
 	/* Bind properties */
 	OBJECT_PROP(AbilitySystemState, state);
-}
-bool AbilitySystem::can_activate_ability(Ref<Ability> ability) const {
-	return is_granted(ability) && has_all_tags(ability->get_tags_required()) && has_no_tags(ability->get_tags_blocking());
+
+	/* Bind signals */
+	ADD_SIGNAL(MethodInfo(as_signal::EventBlocked, OBJECT_PROP_INFO(Ability, ability)));
+	ADD_SIGNAL(MethodInfo(as_signal::EventStarted, OBJECT_PROP_INFO(AbilityEvent, event)));
+	ADD_SIGNAL(MethodInfo(as_signal::EventFinished, OBJECT_PROP_INFO(AbilityEvent, event)));
 }
 
-Ref<AbilityEvent> AbilitySystem::activate_ability(Ref<Ability> ability) {
-	// TODO!
-	auto event = memnew(AbilityEvent);
-	event->set_ability(ability);
-	return event;
+void AbilitySystem::_physics_process(float delta) {
+	if (state.is_valid()) {
+		std::vector<int> finished_events;
+
+		// Tick every event.
+		for_each_i(state->events, [this, delta, &finished_events](Ref<AbilityEvent> event, int i) {
+			// Make a list of finished events.
+			if (event->tick(this, delta) == Status::FINISHED) {
+				finished_events.push_back(i);
+				emit_signal(as_signal::EventFinished, event);
+			}
+		});
+
+		// Remove all finished events.
+		for (int i : finished_events)
+			state->events.remove_at(i);
+	}
 }
 
-bool AbilitySystem::is_granted(Ref<Ability> ability_to_check) const {
+/*************************
+ *** Attribute methods ***
+ *************************/
+
+bool AbilitySystem::has_attribute(Ref<Attribute> attribute) const {
 	ERR_FAIL_NULL_V(state, false);
-	return any(state->granted_abilities, [ability_to_check](Ref<Ability> ability) {
+	return state->attribute_map->has(attribute);
+}
+
+void AbilitySystem::grant_attribute(Ref<Attribute> attribute, float value) {
+	ERR_FAIL_NULL(state);
+	if (!has_attribute(attribute)) {
+		state->attribute_map->add(attribute);
+		state->attribute_map->set_value(attribute, value);
+	}
+}
+
+void AbilitySystem::revoke_attribute(Ref<Attribute> attribute) {
+	ERR_FAIL_NULL(state);
+	if (has_attribute(attribute)) {
+		state->attribute_map->remove(attribute);
+	}
+}
+
+/***********************
+ *** Ability methods ***
+ ***********************/
+
+bool AbilitySystem::has_ability(Ref<Ability> ability_to_check) const {
+	ERR_FAIL_NULL_V(state, false);
+	return any(state->abilities, [ability_to_check](Ref<Ability> ability) {
 		return ability == ability_to_check;
 	});
 }
 
+void AbilitySystem::grant_ability(Ref<Ability> ability) {
+	ERR_FAIL_NULL(state);
+	if (!has_ability(ability)) {
+		state->abilities.append(ability);
+	}
+}
+
+void AbilitySystem::revoke_ability(Ref<Ability> ability) {
+	ERR_FAIL_NULL(state);
+	if (has_ability(ability)) {
+		state->abilities.erase(ability);
+	}
+}
+
+bool AbilitySystem::can_activate(Ref<Ability> ability) const {
+	return has_ability(ability) && has_all_tags(ability->get_tags_required()) && has_no_tags(ability->get_tags_blocking());
+}
+
+Ref<AbilityEvent> AbilitySystem::activate(Ref<Ability> ability) {
+	ERR_FAIL_NULL_V(state, nullptr);
+	if (can_activate(ability)) {
+		auto event = memnew(AbilityEvent);
+		state->events.append(event);
+		event->set_ability(ability);
+		event->start(this);
+		return event;
+	}
+	emit_signal(as_signal::EventBlocked, ability);
+	return nullptr;
+}
+
+/*******************
+ *** Tag methods ***
+ *******************/
+
 bool AbilitySystem::has_tag(Ref<Tag> tag_to_check) const {
 	ERR_FAIL_NULL_V(state, false);
-	return any(state->granted_tags, [tag_to_check](Ref<Tag> tag) {
+	return any(state->tags, [tag_to_check](Ref<Tag> tag) {
 		return tag == tag_to_check;
 	});
 }
@@ -82,4 +143,18 @@ bool AbilitySystem::has_all_tags(TypedArray<Tag> tags_to_check) const {
 bool AbilitySystem::has_no_tags(TypedArray<Tag> tags_to_check) const {
 	ERR_FAIL_NULL_V(state, false);
 	return !has_some_tags(tags_to_check);
+}
+
+void AbilitySystem::grant_tag(Ref<Tag> tag) {
+	ERR_FAIL_NULL(state);
+	if (!has_tag(tag)) {
+		state->tags.append(tag);
+	}
+}
+
+void AbilitySystem::revoke_tag(Ref<Tag> tag) {
+	ERR_FAIL_NULL(state);
+	if (has_tag(tag)) {
+		state->tags.erase(tag);
+	}
 }
--- src/ability_system.h
+++ src/ability_system.h
@@ -7,6 +7,12 @@
 #include "scene/main/node.h"
 #include "tag.hpp"
 
+namespace as_signal {
+const StringName EventBlocked = "ability_event_blocked";
+const StringName EventFinished = "ability_event_finished";
+const StringName EventStarted = "ability_event_started";
+}; //namespace as_signal
+
 class AbilitySystemState : public Resource {
 	GDCLASS(AbilitySystemState, Resource);
 
@@ -14,18 +20,18 @@ class AbilitySystemState : public Resource {
 
 private:
 	Ref<AttributeMap> attribute_map;
-	TypedArray<Tag> granted_tags;
-	TypedArray<Ability> granted_abilities;
-	TypedArray<AbilityEvent> ability_events;
+	TypedArray<Tag> tags;
+	TypedArray<Ability> abilities;
+	TypedArray<AbilityEvent> events;
 
 protected:
 	static void _bind_methods();
 
 public:
 	GETSET_RESOURCE(Ref<AttributeMap>, attribute_map)
-	GETSET_RESOURCE(TypedArray<Tag>, granted_tags)
-	GETSET_RESOURCE(TypedArray<Ability>, granted_abilities)
-	GETSET_RESOURCE(TypedArray<AbilityEvent>, ability_events)
+	GETSET_RESOURCE(TypedArray<Tag>, tags)
+	GETSET_RESOURCE(TypedArray<Ability>, abilities)
+	GETSET_RESOURCE(TypedArray<AbilityEvent>, events)
 };
 
 class AbilitySystem : public Node {
@@ -40,15 +46,30 @@ protected:
 public:
 	GETSET(Ref<AbilitySystemState>, state)
 
-	/* Methods */
+	void _physics_process(float delta);
 
-	bool can_activate_ability(Ref<Ability> ability) const;
-	Ref<AbilityEvent> activate_ability(Ref<Ability> ability);
-	bool is_granted(Ref<Ability> ability_to_check) const;
+	/* Attribute methods */
+
+	bool has_attribute(Ref<Attribute> attribute) const;
+	void grant_attribute(Ref<Attribute> attribute, float value);
+	void revoke_attribute(Ref<Attribute> attribute);
+
+	/* Ability methods */
+
+	bool can_activate(Ref<Ability> ability) const;
+	bool has_ability(Ref<Ability> ability_to_check) const;
+	void grant_ability(Ref<Ability> ability);
+	void revoke_ability(Ref<Ability> ability);
+	Ref<AbilityEvent> activate(Ref<Ability> ability);
+
+	/* Tag methods */
+
 	bool has_tag(Ref<Tag> tag_to_check) const;
 	bool has_some_tags(TypedArray<Tag> tags_to_check) const;
 	bool has_all_tags(TypedArray<Tag> tags_to_check) const;
 	bool has_no_tags(TypedArray<Tag> tags_to_check) const;
+	void grant_tag(Ref<Tag> tag);
+	void revoke_tag(Ref<Tag> tag);
 };
 
 #endif
